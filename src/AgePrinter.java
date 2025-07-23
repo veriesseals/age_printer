ee94dd5a95bdf43bbedb4e8e38ea59d00146eb09
Veries Seals <veries@me.com>
2025-07-23 09:49:53 -0500

    Created AgePrinter Class, defined the int userAge Variable. Imported the Scanner Module to recieve input from the user. Printed the age

---
+++ src/AgePrinter.java
@@ -0,0 +1,20 @@
+// Age Printer
+
+import java.util.Scanner;
+
+public class AgePrinter {
+
+    public static void main(String[] args) {
+        int userAge;
+
+        System.out.print("Enter your age: ");
+        Scanner scnr = new Scanner(System.in);
+        userAge = scnr.nextInt();
+        System.out.println(userAge + " is a great age.");
+
+
+    }
+
+    
+}
+
